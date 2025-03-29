b7548933159520afc85928a74be1b0a5cd2660c2
Hồng Sơn <vansonpham10112k1@gmail.com>
2025-03-29 16:03:03 +0700

    update

--- src/features/User/pages/User/hook.ts
+++ src/features/User/pages/User/hook.ts
@@ -7,8 +7,10 @@ export type ReceivedProps = Record<string, any>;
 const useUser = (props: ReceivedProps) => {
   const dispatch = useAppDispatch();
   const users = useAppSelector(UserSelectors.users);
+  console.log("users", users);
 
   useEffect(() => {
+    console.log("Gọi API lấy danh sách users");
     dispatch(UserActions.getUsers({}));
   }, [dispatch]);
 
--- src/features/User/pages/User/index.tsx
+++ src/features/User/pages/User/index.tsx
@@ -2,16 +2,22 @@ import { FC } from "react";
 import useUser, { Props, ReceivedProps } from "./hook";
 import AppHeader from "@components/AppHeader";
 import { Button } from "@heroui/react";
+import UserTable from "@features/User/component/UserTable";
 
-const UserLayout: FC<Props> = ({ ...props }) => {
+const UserLayout: FC<Props> = ({ users, ...props }) => {
+  console.log("users:", users, Array.isArray(users));
   return (
     <div>
       <AppHeader
         pageTitle="Quản lý người dùng"
         rightMenu={<Button color="primary">Thêm người dùng</Button>}
       />
+      <div className="bg-white rounded-2xl p-4 shadow-md m-4">
+        <UserTable users={users?.data || []} />
+      </div>
     </div>
   );
+  
 };
 
 const User: FC<ReceivedProps> = (props) => <UserLayout {...useUser(props)} />;
